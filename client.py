6a12c41ef42d234afbbd6870925c29207428c8ce
Colin <colinjoe9@gmail.com>
2021-12-12 22:40:21 -0500

    e

--- client.py
+++ client.py
@@ -3,5 +3,9 @@ from valclient import Client
 def join_party(username,password,region,party_id):
     client = Client(region=region,auth={'username':username,'password':password})
     client.activate()
-    print(client.party_join(party_id))
     return client.party_join(party_id)    
+
+def request_party(username,password,region,party_id):
+    client = Client(region=region,auth={'username':username,'password':password})
+    client.activate()
+    return client.party_request(party_id)    
--- main.py
+++ main.py
@@ -33,6 +33,25 @@ def join_party(party_id):
         mimetype='application/json'
     ) 
 
+@app.route('/valorant/request/<party_id>', methods=['POST'])
+def join_party(party_id):
+    json_data = request.get_json()
+    region = request.args.get('region')
+    payload = {}
+    try:
+        data = client.join_party(json_data['username'],json_data['password'],region,party_id)
+        payload = data
+    except:
+        payload = {
+            'error': 'Unable to request to join party.'
+        }
+    
+    return Response(
+        response=json.dumps(payload),
+        status=200,
+        mimetype='application/json'
+    ) 
+
 
 # run the app.
 if __name__ == "__main__":
